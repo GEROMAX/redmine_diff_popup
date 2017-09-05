04a8b4a528dd9835d4bc74e29494a292d250d84b
GEROMAX <tobita0112@gmail.com>
2017-09-05 18:05:39 +0900

    add script

--- assets/javascripts/diff_popup.js
+++ assets/javascripts/diff_popup.js
@@ -1,31 +1,42 @@
-/*difftest*/
-$(function () {
-  $("#history div.journal ul > li > a").attr("onclick", "return false;").on("click", function () {
-    var diffUrl = $(this).attr("href");
-    var clickNoteId = $(this).parents("div.journal:first").find("h4 > a.journal-link").text();
-    var dlgName = "#diffpopup" + clickNoteId.split("#")[1];
-    if ($(dlgName).size() == 1)
-    {
-      $(dlgName).dialog("moveToTop");
-      return;
-    }
-    $.get(diffUrl, { noteId: clickNoteId }, function (data) {
-      var noteName = decodeURIComponent(this.url.split("?")[1].split("&")[1].split("=")[1]);
-      var popupId = "diffpopup" + noteName.split("#")[1];
-      $("#content").append("<div id='" + popupId + "'></div>");
-      $("#" + popupId).html($("#content", data).html()).find("h2, p:nth-child(4) > a").remove().end().dialog({
-        title: noteName,
-        width: window.innerWidth / 2 - 20,
-        maxHeight: window.innerHeight - 40,
-        position: { my: "center center", at: "right center", of: window },
-        open: function () {
-          popupOffset = $("#" + popupId).offset();
-        },
-        close: function (event) {
-          $(this).dialog('destroy');
-          $(event.target).remove();
-        }
-      }).show();
+function replaceJournalDiff()
+{
+  $("#history div.journal ul > li > a").each(function(){
+    //find li
+    var orgDiff = $(this).parent();
+    var popDiff = $(this).parents("div.journal:first").next("li");
+
+    //replace and set func
+    popDiff.replaceAll(orgDiff).find("a").on("click", function(){
+      var diffUrl = $(this).attr("href");
+      var journalIndice = diffUrl.split("&indice=")[1];
+      var dlgName = "#diffpopup" + journalIndice;
+      
+      //on top dialog
+      if ($(dlgName).size() == 1)
+      {
+        $(dlgName).dialog("moveToTop");
+        return;
+      }
+      
+      //show dialog
+      $.get(diffUrl, function (data) {
+        var journalIndice = decodeURIComponent(this.url.split("?")[1].split("&indice=")[1]);
+        var popupId = "diffpopup" + journalIndice;
+        $("#content").append("<div id='" + popupId + "'></div>");
+        $("#" + popupId).html(data).dialog({
+          title: "#" + journalIndice,
+          width: window.innerWidth / 2 - 20,
+          maxHeight: window.innerHeight - 80,
+          position: { my: "center center", at: "right center", of: window },
+          open: function () {
+            popupOffset = $("#" + popupId).offset();
+          },
+          close: function (event) {
+            $(this).dialog('destroy');
+            $(event.target).remove();
+          }
+        }).show();
+      });
     });
   });
-});
+}
