a756f0d7ce7b9ff284280ff7b1a0e1f664fd9ee8
GEROMAX <tobita0112@gmail.com>
2017-09-12 10:03:51 +0900

    add enable/disable switch by user preference

--- lib/patches/issues_helper_patch.rb
+++ lib/patches/issues_helper_patch.rb
@@ -25,9 +25,7 @@ module RedmineDiffPopup
         label = detail.prop_key == 'description' ? l(:field_description) : detail.custom_field.name
         label = content_tag('strong', label)
         s = l(:text_journal_changed_no_detail, :label => label)
-        diff_link = link_to 'diffpop',
-        #for debug
-        #diff_link = link_to 'diff',
+        diff_link = link_to 'diff',
           diff_popup_journal_diff_url(:id => detail.journal_id, :detail_id => detail.id, :indice => indice),
           :title => l(:label_view_diff),
           :onclick => 'return false;'
