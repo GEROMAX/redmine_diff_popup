be7aff31759223cbf29ea536c061cd969fbf8a55
GEROMAX <tobita0112@gmail.com>
2017-12-14 18:09:06 +0900

    development at 4th.

--- app/controllers/wiki_popup_controller.rb
+++ app/controllers/wiki_popup_controller.rb
@@ -1,11 +1,12 @@
 class WikiPopupController < ApplicationController
   unloadable
   default_search_scope :wiki_pages
+  before_action :find_wiki, :authorize
   before_filter :find_existing_page, :only => [:wiki_diff]
 
   layout false
-  helper :issues
-  helper :custom_fields
+  # helper :issues
+  # helper :custom_fields
 
   def wiki_diff
     @diff = @page.diff(params[:version], params[:version_from])
@@ -14,6 +15,14 @@ class WikiPopupController < ApplicationController
 
 private
 
+  def find_wiki
+    @project = Project.find(params[:project_id])
+    @wiki = @project.wiki
+    render_404 unless @wiki
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+
   # Finds the requested page and returns a 404 error if it doesn't exist
   def find_existing_page
     @page = @wiki.find_page(params[:id])
@@ -21,9 +30,9 @@ private
       render_404
       return
     end
-    if @wiki.page_found_with_redirect?
-      redirect_to_page @page
-    end
+    # if @wiki.page_found_with_redirect?
+    #   redirect_to_page @page
+    # end
   end
 
 end
--- lib/redmine_diff_popup/hooks/diff_popup_hook.rb
+++ lib/redmine_diff_popup/hooks/diff_popup_hook.rb
@@ -1,6 +1,7 @@
 class DiffPopupHook < Redmine::Hook::ViewListener
   render_on :view_issues_history_journal_bottom, :partial => 'issues/issues_history_journal_bottom_partial'
   render_on :view_issues_show_details_bottom, :partial => 'issues/issues_show_details_bottom_partial'
-  render_on :view_layouts_base_content, :partial => 'layouts/wiki_history_partial'
+  render_on :view_layouts_base_html_head, :partial => 'layouts/wiki_popup_html_head_partial'
+  render_on :view_layouts_base_body_bottom, :partial => 'layouts/wiki_popup_body_bottom_partial'
   render_on :view_my_account_preferences, :partial => 'my/diff_popup_preferences'
 end
