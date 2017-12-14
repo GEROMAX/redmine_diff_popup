1189e99f4d8ab6e7acfbc131e8558cdd4fce6964
GEROMAX <tobita0112@gmail.com>
2017-12-14 16:00:38 +0900

    development at 2nd.

--- app/controllers/wiki_popup_controller.rb
+++ app/controllers/wiki_popup_controller.rb
@@ -12,25 +12,6 @@ class WikiPopupController < ApplicationController
     render_404 unless @diff
   end
 
-  # def journal_diff
-  #   @issue = @journal.issue
-  #   if params[:detail_id].present?
-  #     @detail = @journal.details.find_by_id(params[:detail_id])
-  #   else
-  #     @detail = @journal.details.detect {|d| d.property == 'attr' && d.prop_key == 'description'}
-  #   end
-  #   unless @issue && @detail
-  #     render_404
-  #     return false
-  #   end
-  #   if @detail.property == 'cf'
-  #     unless @detail.custom_field && @detail.custom_field.visible_by?(@issue.project, User.current)
-  #       raise ::Unauthorized
-  #     end
-  #   end
-  #   @diff = Redmine::Helpers::Diff.new(@detail.value, @detail.old_value)
-  # end
-
 private
 
   # Finds the requested page and returns a 404 error if it doesn't exist
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get 'diff_popup/journal_diff', to: 'diff_popup#journal_diff'
-  get 'diff_popup/wiki_diff', to: 'diff_popup#wiki_diff'
+  get 'wiki_popup/wiki_diff', to: 'wiki_popup#wiki_diff'
 end
