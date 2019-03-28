f3e3cf2b62ff6eb686a55d6730d7224e91517559
Gui Shaoli <guishaoli@tp-link.com.cn>
2019-03-28 09:41:47 +0800

    make diff nice to see

--- app/controllers/diff_popup_controller.rb
+++ app/controllers/diff_popup_controller.rb
@@ -1,3 +1,4 @@
+require 'htmldiff'
 class DiffPopupController < ApplicationController
   unloadable
   before_action :find_journal, :only => [:journal_diff]
@@ -23,6 +24,14 @@ class DiffPopupController < ApplicationController
       end
     end
     @diff = Redmine::Helpers::Diff.new(@detail.value, @detail.old_value)
+
+    # change start,tplink,guishaoli
+    # 使用edouard-htmldiff对issue的修改前后的内容进行diff
+    @diff_result = HTMLDiff.diff(@detail.old_value,@detail.value,true)
+    # 将原生的diffmod（修改）diffdel（删除） diffins（增加）替换成redmine的class
+    @left = @diff_result[0].gsub("diffmod","diff_out").gsub("difftype","class").gsub("diffdel","diff_out")
+    @right = @diff_result[1].gsub("diffmod","diff_in").gsub("difftype","class").gsub("diffins","diff_in")
+    # change end
   end
 
 private
